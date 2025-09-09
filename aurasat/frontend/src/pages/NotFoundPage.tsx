f58865b54e12dd344885533862a860409c91b9a7
Ed-Amael <mysafe.wolf@gmail.com>
2025-09-10 00:07:00 +0530

    Delete aurasat directory

--- aurasat/frontend/src/pages/NotFoundPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFoundPage() {
-  return (
-    <div className="card">
-      <h2>Page not found</h2>
-      <p className="muted">The page you are looking for doesn’t exist.</p>
-    </div>
-  );
-}
